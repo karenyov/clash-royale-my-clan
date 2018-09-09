17e264d443e2084af58d2cd0074cf40fa5ee5fec
Karen Vicente <karenyasmin37@gmail.com>
2018-09-09 18:27:45 -0300

    initial commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/clash-royale-my-clan'));
+app.use(express.static('./clash-royale-my-clan/dist'));
 
 app.get('/*all', function(req,res) {
     
-res.sendFile(path.join('./dist/clash-royale-my-clan/index.html'));
+res.sendFile(path.join('./clash-royale-my-clan/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
