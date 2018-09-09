2deb3cb290228f38f4a7b12070bbea39e86ad316
Karen Vicente <karenyasmin37@gmail.com>
2018-09-09 17:33:10 -0300

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.royaleapi.com',
+  apiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQ2MywiaWRlbiI6IjQ3ODkzMjgzNjA0NTI5MTUzMCIsIm1kIjp7InVzZXJuYW1lIjoiS2FyZW4iLCJrZXlWZXJzaW9uIjozLCJkaXNjcmltaW5hdG9yIjoiMTgyNSJ9LCJ0cyI6MTUzNDI4MTY4NTY1MH0.0bTFu0bfi1bOd9BI-YaWEPnXocYSKmawQcGmlejvFyU',
+  clanTag: 'UVRVYQ'
 };
